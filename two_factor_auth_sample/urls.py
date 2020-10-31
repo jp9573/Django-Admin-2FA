c09a867095c1eb64471e2eb02eca62794ecfe47d
jp9573 <jp9573@gmail.com>
2020-10-31 15:58:34 +0530

    Configure Admin Interface

--- two_factor_auth_sample/urls.py
+++ two_factor_auth_sample/urls.py
@@ -15,6 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django_otp.admin import OTPAdminSite
+from django.contrib.auth.models import User
+from django_otp.plugins.otp_totp.models import TOTPDevice
+from django_otp.plugins.otp_totp.admin import TOTPDeviceAdmin
+
+
+class OTPAdmin(OTPAdminSite):
+    pass
+
+
+admin_site = OTPAdmin(name='OTPAdmin')
+admin_site.register(User)
+admin_site.register(TOTPDevice, TOTPDeviceAdmin)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
